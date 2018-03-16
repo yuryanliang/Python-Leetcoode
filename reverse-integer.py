90876eb05fc9b025615d0f55a0f7342a78a7023c
Ryan Liang <31252603+yuryanliang@users.noreply.github.com>
2018-03-15 22:55:50 -0700

    Create reverse-integer.py

---
+++ reverse-integer.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        answer = 0
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        while x > 0:
+            answer = answer * 10 + x % 10
+            x /= 10
+        return sign*answer if answer <= 0x7fffffff else 0 # Handle overflow.
