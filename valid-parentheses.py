4b909704e7c63e09e8cc238c6c0d73b7ecea139a
Ryan Liang <31252603+yuryanliang@users.noreply.github.com>
2018-03-17 02:08:15 -0700

    Create valid-parentheses.py

---
+++ valid-parentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack =[]
+        for i in range(len(s)):
+            if s[i] =='(' or s[i]=='[' or s[i]=='{':
+                stack.append(s[i])
+            if s[i] == ')':
+                if stack == [] or stack.pop()!='(':
+                    return False
+            if s[i] == ']':
+                if stack == [] or stack.pop()!='[':
+                    return False
+            if s[i] == '}':
+                if stack == [] or stack.pop()!='{':
+                    return False
+        if stack !=[]:
+            return False
+        else:
+            return True
+                
+                
