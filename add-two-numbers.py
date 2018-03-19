1e2355786ae2cadc899fd35ecde8fd2783935e3d
Ryan Liang <31252603+yuryanliang@users.noreply.github.com>
2018-03-18 22:34:25 -0700

    Create add-two-numbers.py

---
+++ add-two-numbers.py
@@ -0,0 +1,63 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# You are given two linked lists representing two non-negative numbers. 
+# The digits are stored in reverse order and each of their nodes contain a single digit.
+# Add the two numbers and return it as a linked list.
+#
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+#
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        
+        flag = 0
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while l1 and l2:
+            sum= l1.val + l2.val + flag
+            curr.next = ListNode(sum % 10)
+            flag = sum / 10
+            l1 = l1.next
+            l2 = l2.next
+            curr = curr.next
+        
+        while l1:
+            sum = l1.val + flag
+            curr.next = ListNode (sum % 10)
+            flag = sum / 10
+            l1 = l1.next
+            curr = curr.next
+            
+        while l2:
+            sum = l2.val + flag
+            curr.next = ListNode (sum % 10)
+            flag = sum / 10
+            l2 = l2.next
+            curr = curr.next
+        
+        if flag == 1:
+            curr.next = ListNode(1)
+        return dummy.next
+
+if __name__ == '__main__':
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    result = Solution().addTwoNumbers(a, b)
+    print "{0} -> {1} -> {2}".format(result.val, result.next.val, result.next.next.val)
