103eb2afafaa98e20a9eb283ecd783b34f865c53
Ryan Liang <31252603+yuryanliang@users.noreply.github.com>
2018-03-26 00:30:12 -0700

    Update Two_sum_II.py

--- Two_sum_II.py
+++ Two_sum_II.py
@@ -32,3 +32,18 @@ class Solution:
             else:
                 return [left+1, right+1]
                 
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map={}
+        
+        for i  in range(len(numbers)):
+            if target - numbers[i] in num_map:
+                return (num_map[target - numbers[i]]+1, i+1)
+            else:
+                num_map[numbers[i]] = i
+        
